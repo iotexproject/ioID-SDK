e43cf64c71fc15b451d687d7efeac29185bbd366
Jianian.Zhang <46586581+iloveyou-github@users.noreply.github.com>
2026-01-07 08:11:37 +0800

    Update pro_config.h

--- example/linux/solarfarm/pro_config.h
+++ example/linux/solarfarm/pro_config.h
@@ -2,7 +2,7 @@
 #define __IOTEX_PAL_CONFIG_H__
 
 #define IOTEX_SOLAR_FARM_DEMO_HTTP_HOST         "https://dragonfruit-mainnet.w3bstream.com/v1/task"
-#define IOTEX_SOLAR_FARM_DEMO_PROJECT_ID        "1009"
+#define IOTEX_SOLAR_FARM_DEMO_PROJECT_ID        "delen-test"
 
 #define IOTEX_SOLAR_FARM_DEMO_TEST              0
 
